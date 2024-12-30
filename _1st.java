7c44b4a4def5ee3b2ac004ca317ad683b92643c6
Ukasha <116743795+Ukashashere@users.noreply.github.com>
2024-12-30 19:26:59 +0530

    1

---
+++ _1st.java
@@ -0,0 +1,40 @@
+public class _1st {
+    public static void main(String[] args) {
+        System.out.println("Ukasha");
+    }
+    void uka (int n){
+        n = 10;
+        for(int i = 0; i< n; i++ ){
+            return(i);
+        }
+    }    
+}
+
+/*
+public class _1st {
+
+    int uka (int n){
+        n = 10;
+        for(int i = 0; i< n; i++ ){
+            System.out.println(n);
+        }
+}
+*/
+
+
+/* 
+Steps to Run:
+Save the file with the same name as the class: _1st.java.
+Compile the code:
+javac _1st.java
+Run the compiled program:
+java _1st
+If you want the file to remain named 1st.java, ensure the class name matches the file name:
+
+public class _1st {
+    public static void main(String[] args) {
+        System.out.println("ty");
+    }
+}
+Note: Java class names cannot start with a digit, so you'll need to modify the class name if the file is 1st.java.
+*/
