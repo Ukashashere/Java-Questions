e50e45d1cae4a1adac426bd82cad587ac05f53ef
Ukasha <116743795+Ukashashere@users.noreply.github.com>
2024-12-30 19:32:42 +0530

    2

---
+++ _2.java
@@ -0,0 +1,17 @@
+public class _2 {
+    public static void main(String[] args){
+        void uka (int n){
+            n = 10;
+            for(int i = 0; i< n; i++ ){
+                System.out.println(i);
+            }
+        }
+
+    }
+    void uka (int n){
+        n = 10;
+        for(int i = 0; i< n; i++ ){
+            System.out.println(i);
+        }
+    } 
+}
