7c3e69628df7b80cd009b5349d513b27e511a166
Ukasha <116743795+Ukashashere@users.noreply.github.com>
2024-12-31 18:35:04 +0530

    3.3 Working

--- _3.java
+++ _3.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class _3 {
     public static int f(int n) {
         if (n <= 1) {
             return 1;
         }
         return f(n - 1) + f(n - 1);
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Result: " + f(n));
+
+        scanner.close();
     }
 }
 
