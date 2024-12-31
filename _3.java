4f72107575a599302ec6a1f8f4940a735fe818b9
Ukasha <116743795+Ukashashere@users.noreply.github.com>
2024-12-31 18:18:55 +0530

    3

---
+++ _3.java
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+int f(int n){
+    if (n <= 1){
+        return 1;
+    }
+    return f(n - 1) + f(n - 1);
+}
+
+Normal Code ☝️
+Actually hava to write like ☝️☝️☝️
+*/
